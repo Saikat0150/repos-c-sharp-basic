667d64b5c1596d161537982421cccb8f4015c4a5
Saikat0150 <saikat.sanil.0150@gmail.com>
2022-02-28 12:26:56 +0530

    Change

---
+++ Using Multiple namespace with same class name/Using Multiple namespace with same class name/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Using_Multiple_namespace_with_same_class_name
+{
+    namespace first_space
+    {
+        class namespace_cl
+        {
+            public void func()
+            {
+                Console.WriteLine("Using First Namespace");
+            }
+        }
+    }
+    namespace second_space
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        class namespace_cl
+        {
+            public void func()
+            {
+                Console.WriteLine("Using Second Namespace");
+            }
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            first_space.namespace_cl f = new first_space.namespace_cl();
+            second_space.namespace_cl s = new second_space.namespace_cl();
+            f.func();
+            s.func();
+            Console.Read();
+        }
+    }
+}
